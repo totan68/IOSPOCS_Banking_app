52cf466658d878bb26cb01f06dd675b09c80d24a
totan68 <164209696+totan68@users.noreply.github.com>
2024-08-05 17:51:00 +0530

    Initial Commit

---
+++ IOSPOCS_Banking_app/IOSPOCS_Banking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSPOCS_Banking_appApp.swift
+//  IOSPOCS_Banking_app
+//
+//  Created by Totan Hazra on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IOSPOCS_Banking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
